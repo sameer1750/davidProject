86d9ea563ba27026aff2ec0b7d9b4677f7c63943
sameer shaikh <sameer@firstnotify.in>
2017-05-24 14:10:31 +0530

    M

--- ediffy-classes/resources/views/layouts/login.blade.php
+++ ediffy-classes/resources/views/layouts/login.blade.php
@@ -5,14 +5,14 @@
     <title>Ediffy Admin - @yield('title')</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.4 -->
-    <link href="/bower_components/admin-lte/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/classes/bower_components/admin-lte/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
     <!-- Font Awesome Icons -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"
           type="text/css"/>
     <!-- Theme style -->
-    <link href="/bower_components/admin-lte/dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/classes/bower_components/admin-lte/dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css"/>
     <!-- iCheck -->
-    <link href="/bower_components/admin-lte/plugins/iCheck/square/red.css" rel="stylesheet" type="text/css"/>
+    <link href="/classes/bower_components/admin-lte/plugins/iCheck/square/red.css" rel="stylesheet" type="text/css"/>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -32,11 +32,11 @@
 <!-- /.login-box -->
 
 <!-- jQuery 2.1.4 -->
-<script src="/bower_components/admin-lte/plugins/jQuery/jQuery-2.1.4.min.js"></script>
+<script src="/classes/bower_components/admin-lte/plugins/jQuery/jQuery-2.1.4.min.js"></script>
 <!-- Bootstrap 3.3.2 JS -->
-<script src="/bower_components/admin-lte/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="/classes/bower_components/admin-lte/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
 <!-- iCheck -->
-<script src="/bower_components/admin-lte/plugins/iCheck/icheck.min.js" type="text/javascript"></script>
+<script src="/classes/bower_components/admin-lte/plugins/iCheck/icheck.min.js" type="text/javascript"></script>
 <script>
     $(function () {
         $('input').iCheck({
--- ediffy-classes/resources/views/layouts/master.blade.php
+++ ediffy-classes/resources/views/layouts/master.blade.php
@@ -5,22 +5,22 @@
     <title>Edify Admin @yield('title')</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.4 -->
-    <link href="/bower_components/admin-lte/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/classes/bower_components/admin-lte/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
     <!-- FontAwesome 4.3.0 -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"
           type="text/css"/>
     <!-- Ionicons 2.0.0 -->
     <link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css"/>
-    <link href="/bower_components/admin-lte/plugins/iCheck/square/blue.css" rel="stylesheet" type="text/css"/>
+    <link href="/classes/bower_components/admin-lte/plugins/iCheck/square/blue.css" rel="stylesheet" type="text/css"/>
     <!-- Date Picker -->
-    <link rel="stylesheet" href="/bower_components/admin-lte/plugins/datepicker/datepicker3.css">
-    <link href="/bower_components/admin-lte/plugins/daterangepicker/daterangepicker-bs3.css" rel="stylesheet"
+    <link rel="stylesheet" href="/classes/bower_components/admin-lte/plugins/datepicker/datepicker3.css">
+    <link href="/classes/bower_components/admin-lte/plugins/daterangepicker/daterangepicker-bs3.css" rel="stylesheet"
           type="text/css"/>
     <link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" rel="stylesheet"/>
     <!-- Theme style -->
-    <link href="/bower_components/admin-lte/dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css"/>
-    <link href="/bower_components/admin-lte/dist/css/skins/skin-blue.min.css" rel="stylesheet" type="text/css"/>
-    <link href="/bower_components/admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" type="text/css"  rel="stylesheet" >
+    <link href="/classes/bower_components/admin-lte/dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/classes/bower_components/admin-lte/dist/css/skins/skin-blue.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/classes/bower_components/admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" type="text/css"  rel="stylesheet" >
     <!-- iCheck -->
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -28,7 +28,7 @@
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <script src="/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.js"></script>
+    <script src="/classes/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.js"></script>
 
     <![endif]-->
 </head>
@@ -63,41 +63,41 @@
 <!-- ./wrapper -->
 
 <!-- jQuery 2.1.4 -->
-<script src="/bower_components/admin-lte/plugins/jQuery/jQuery-2.1.4.min.js"></script>
+<script src="/classes/bower_components/admin-lte/plugins/jQuery/jQuery-2.1.4.min.js"></script>
 
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 {{--<script>--}}
 {{--$.widget.bridge('uibutton', $.ui.button);--}}
 {{--</script>--}}
 <!-- Bootstrap 3.3.2 JS -->
-<script src="/bower_components/admin-lte/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="/classes/bower_components/admin-lte/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
 
 <!-- Sparkline -->
-<script src="/bower_components/admin-lte/plugins/sparkline/jquery.sparkline.min.js" type="text/javascript"></script>
+<script src="/classes/bower_components/admin-lte/plugins/sparkline/jquery.sparkline.min.js" type="text/javascript"></script>
 <!-- jvectormap -->
-<script src="/bower_components/admin-lte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"
+<script src="/classes/bower_components/admin-lte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"
         type="text/javascript"></script>
-<script src="/bower_components/admin-lte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"
+<script src="/classes/bower_components/admin-lte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"
         type="text/javascript"></script>
 <!-- jQuery Knob Chart -->
-<script src="/bower_components/admin-lte/plugins/knob/jquery.knob.js" type="text/javascript"></script>
+<script src="/classes/bower_components/admin-lte/plugins/knob/jquery.knob.js" type="text/javascript"></script>
 <!-- daterangepicker -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js" type="text/javascript"></script>
-<script src="/bower_components/admin-lte/plugins/daterangepicker/daterangepicker.js" type="text/javascript"></script>
+<script src="/classes/bower_components/admin-lte/plugins/daterangepicker/daterangepicker.js" type="text/javascript"></script>
 <!-- datepicker -->
-<script src="/bower_components/admin-lte/plugins/datepicker/bootstrap-datepicker.js" type="text/javascript"></script>
+<script src="/classes/bower_components/admin-lte/plugins/datepicker/bootstrap-datepicker.js" type="text/javascript"></script>
 <!-- Bootstrap WYSIHTML5 -->
-<script src="/bower_components/admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"
+<script src="/classes/bower_components/admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"
         type="text/javascript"></script>
 <!-- Slimscroll -->
-<script src="/bower_components/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+<script src="/classes/bower_components/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>
 <!-- FastClick -->
-<script src='/bower_components/admin-lte/plugins/fastclick/fastclick.min.js'></script>
+<script src='/classes/bower_components/admin-lte/plugins/fastclick/fastclick.min.js'></script>
 <!-- AdminLTE App -->
-<script src="/bower_components/admin-lte/dist/js/app.min.js" type="text/javascript"></script>
+<script src="/classes/bower_components/admin-lte/dist/js/app.min.js" type="text/javascript"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
-<script src="/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
-<script src="/bower_components/admin-lte/plugins/datepicker/bootstrap-datepicker.js"></script>
+<script src="/classes/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
+<script src="/classes/bower_components/admin-lte/plugins/datepicker/bootstrap-datepicker.js"></script>
 
 @yield('scripts')
 </body>
